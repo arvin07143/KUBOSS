0f346f38f526699e51fa83080acf082f55930b34
arvin07143 <arvin07143@gmail.com>
2021-04-14 13:31:00 +0800

    save qr code TODO: Print Intent

--- app/src/main/java/com/example/kuboss/warehouse/ReceiveItemFragment.kt
+++ app/src/main/java/com/example/kuboss/warehouse/ReceiveItemFragment.kt
@@ -1,11 +1,17 @@
 package com.example.kuboss.warehouse
 
+import android.app.Activity
+import android.content.ContentValues
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -16,6 +22,9 @@ import com.example.kuboss.databinding.FragmentReceiveItemBinding
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
 import com.google.zxing.qrcode.QRCodeWriter
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.util.*
 
 class ReceiveItemFragment : Fragment() {
 
@@ -49,6 +58,7 @@ class ReceiveItemFragment : Fragment() {
 
             val img: Bitmap = getQrCodeBitmap(receiveId, receiveSKU, receiveName, receiveQty.toString())
             binding.qrView.setImageBitmap(img)
+            saveImage(img, this.activity!!,receiveId)
         }
         return binding.root
     }
@@ -72,6 +82,32 @@ class ReceiveItemFragment : Fragment() {
                 }
             }
         }
+    }
+
+    fun saveImage(bmpImage:Bitmap, activity: Activity,fileName:String) {
+
+        ByteArrayOutputStream().apply {
+            bmpImage.compress(Bitmap.CompressFormat.JPEG, 100, this)
+        }
+
+        val imageFile =  File("${activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)},$fileName+QR.jpg/")
+
+        if (!imageFile.exists()) {
+
+            val contentResolver = ContentValues().apply {
+                put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis())
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put(MediaStore.Images.Media.DATA, imageFile.absolutePath)
+            }
+
+            activity.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentResolver).apply {
+                bmpImage.compress(Bitmap.CompressFormat.JPEG, 100, activity.contentResolver.openOutputStream(this!!))
+            }
+
+
+            Toast.makeText(activity, "saved", Toast.LENGTH_SHORT).show()
+        } else
+            Toast.makeText(activity, "Already saved", Toast.LENGTH_SHORT).show()
     }
 
 
